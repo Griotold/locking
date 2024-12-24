cae75175447df9ff085715ec8230af3c0c6f298e
Griotold <gotjd9773@naver.com>
2024-12-24 16:26:20 +0900

    데드락

--- src/main/java/com/example/locking/item/ItemService.java
+++ src/main/java/com/example/locking/item/ItemService.java
@@ -2,6 +2,7 @@ package com.example.locking.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -20,6 +21,22 @@ public class ItemService {
 
         // 수정된 데이터를 저장합니다.
         itemRepository.save(item);
+    }
+
+    @Transactional(timeout = 1, isolation = Isolation.SERIALIZABLE)
+    public void updateItemsQuantity(Long itemId1, Long itemId2) {
+        // 첫 번째 아이템에 락을 건 후 업데이트
+        Item item1 = itemRepository.findByIdWithLock(itemId1);
+        item1.setQuantity(item1.getQuantity() + 10);
+        itemRepository.save(item1);
+
+        // 잠시 대기
+        try { Thread.sleep(4000); } catch (InterruptedException e) { e.printStackTrace(); }
+
+        // 두 번째 아이템에 락을 건 후 업데이트
+        Item item2 = itemRepository.findByIdWithLock(itemId2);
+        item2.setQuantity(item2.getQuantity() + 10);
+        itemRepository.save(item2);
     }
 
     @Transactional
